54a9adef017e7a503793165a43e922a6f3588aec
Dipesh Yadav <73176903+techboydk@users.noreply.github.com>
2022-08-16 16:31:12 +0530

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded',init,false);
+
+function init(){
+  const flag = document.querySelector('.flag');
+  let w = flag.offsetWidth
+  console.log(flag)
+  for (let i = 0; i < w; i++) {
+    const flagElement = document.createElement('div');
+    flagElement.setAttribute('class','flag-element');
+    flagElement.style.backgroundPosition = `${-i}px 0`
+    flagElement.style.animationDelay = `${i*20}ms`
+    flag.append(flagElement);
+  }
+}
+
+
 
